81e01d1cdb8417ae67e690c77f754240508d92f2
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-07-29 20:30:09 -0300

    finalizado

--- DesafioBusca/src/main/java/org/example/Main.java
+++ DesafioBusca/src/main/java/org/example/Main.java
@@ -6,14 +6,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner leitura = new Scanner(System.in);
+        Scanner leitura = new Scanner(System.in).useDelimiter("\n");;
         List<Pessoa> pessoas = new ArrayList<>();
-        List<Endereco> enderecos= new ArrayList<>();
 
 
         System.out.println("Digite a quantidade de pessoas a serem cadastradas: ");
         int qntd_pessoas = leitura.nextInt();
         for (int i = 1; i <= qntd_pessoas; i++) {
+            List<Endereco> enderecos= new ArrayList<>();
+            Pessoa pessoa;
             System.out.println(String.format("\nCadastro de Pessoa [ %s ] - Digite o nome e telefone, respectivamente, separados por ENTER: ", i));
             String nome = leitura.next();
             String telefone = leitura.next();
@@ -21,21 +22,19 @@ public class Main {
             int qntd_enderecos = leitura.nextInt();
             for (int j = 1; j <= qntd_enderecos; j++) {
                 System.out.println(String.format("\nEndereço [ %s ] - Digite o logradouro, cidade e estado, respectivamente, separados por ENTER: ", j));
-                String logradouro = leitura.nextLine();
-                String cidade = leitura.nextLine();
-                String estado = leitura.nextLine();
+                String logradouro = leitura.next();
+                String cidade = leitura.next();
+                String estado = leitura.next();
                 enderecos.add(new Endereco(logradouro ,cidade, estado));
             }
             pessoas.add(new Pessoa(nome, telefone, enderecos));
-            enderecos.clear();
         }
         for (Pessoa pessoa:
                 pessoas) {
-            System.out.println(" - "+ pessoa.getNome() + " - Telefone: "+ pessoa.getTelefone());
-            System.out.println(" - Enderecos: ");
+            System.out.println(String.format("- Nome: %s \n- Telefone: %s \n- Endereços:", pessoa.getNome(), pessoa.getTelefone()));
             for (Endereco end:
                     pessoa.getEnderecos()) {
-                System.out.println(" --- "+ end.getLogradouro() + " - " + end.getCidade() + " - " + end.getEstado());
+                System.out.println(String.format(" --- %s , %s - %s", end.getLogradouro(), end.getCidade(), end.getEstado()));
             }
             System.out.println("\n");
         }
